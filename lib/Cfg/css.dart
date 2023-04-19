774bbc66c386b3fef814529e519f2a4b22cff761
tuk4ngketik <tuk4ng.ketik@gmail.com>
2023-04-19 09:57:06 +0700

    menysuaikan bg putih

--- lib/Cfg/css.dart
+++ lib/Cfg/css.dart
@@ -6,21 +6,29 @@ class Css {
   static TextStyle titlePage  = const TextStyle(color: Colors.black, fontSize: 20);
 
 
-  // Form
+  // Formc 
+  static OutlineInputBorder roundInput20 = const OutlineInputBorder(
+                      borderSide: BorderSide(width:0, strokeAlign: 0, color: Colors.white),
+                      borderRadius: BorderRadius.all(Radius.circular(20)),  
+                    ); 
+
   static OutlineInputBorder round20 = const OutlineInputBorder(
                       borderSide: BorderSide(width: 0.1, strokeAlign: 0.5),
                       borderRadius: BorderRadius.all(Radius.circular(20)),  
                     ); 
 
-  
+   
+
   static TextStyle labelStyle = const TextStyle(color: Colors.black, fontSize: 18);
 
   static TextStyle profilText = const TextStyle(color: Colors.black, fontSize: 17);
 
   // Harga
-  static TextStyle labelHarga = const TextStyle(color: Colors.white70, fontSize: 17);
+  static TextStyle labelHarga = const TextStyle(color: Colors.black, fontSize: 17); 
 
-  static TextStyle labelTarif= const  TextStyle( color: Colors.white,   fontWeight: FontWeight.bold, letterSpacing: 1, fontSize: 18,   );
+  static TextStyle labelTarif= const  TextStyle( color: Colors.black,   fontWeight: FontWeight.bold, letterSpacing: 1, fontSize: 18,   );
+  
+  static TextStyle textTotal = const TextStyle(color: Colors.white, fontSize: 18); 
 
   // Bottom Bar
   static OutlineInputBorder roundBottomAppbar = const OutlineInputBorder(
